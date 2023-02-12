05038b3aefc4037eff77c588d4500cb9b4ada39c
hanjoo0211 <hanjoo0211@gmail.com>
2023-02-12 20:49:44 +0900

    23.2.12

---
+++ 23-2-2/2738.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+b = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        a[i][j] += b[i][j]
+
+for i in range(n):
+    r = ''
+    for j in range(m):
+        r += str(a[i][j]) + ' '
+    print(r[:-1])
