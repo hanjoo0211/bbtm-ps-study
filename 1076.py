9d9a618d8ca31905d47c0b6d07e724a03426d4e0
hanjoo0211 <hanjoo0211@gmail.com>
2023-04-28 23:47:04 +0900

    23.4.28

---
+++ 1076.py
@@ -0,0 +1,17 @@
+color = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
+value = color[input()]
+value *= 10
+value += color[input()]
+value *= 10**color[input()]
+print(value)
